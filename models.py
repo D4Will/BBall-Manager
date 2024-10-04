acef090a281dcd708003be3385b018e3002c62a5
D4Will <willydouglass@icloud.com>
2024-10-04 15:35:43 -0500

    updated datapase path to heroku

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 
 # These are variables used to connect flask application to local database.
-database_name = 'bball_manager'
-database_path = 'postgresql://{}:{}@{}/{}'.format('postgres', 'abc', 'localhost:5432', database_name)
+# database_name = 'bball_manager'
+# database_path = 'postgresql://{}:{}@{}/{}'.format('postgres', 'abc', 'localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
